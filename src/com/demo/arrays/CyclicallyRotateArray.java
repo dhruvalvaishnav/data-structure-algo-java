927699d8559adf03bd910772bff1e4057c796f31
Dhruval Vaishnav <dvaishnav@adobe.com>
2024-09-14 12:33:00 +0530

    Cyclically Rotating the array

--- src/com/demo/arrays/CyclicallyRotateArray.java
+++ src/com/demo/arrays/CyclicallyRotateArray.java
@@ -14,39 +14,61 @@ public class CyclicallyRotateArray {
         System.out.println("*************************************************");
 
         System.out.println("Given Array is = " + Arrays.toString(new int[]{1, 2, 3, 4, 5, 6, 7}));
-        leftRotate(arr, 4, 7);
-        System.out.println("Left Rotated Array from Element " + 4 + " is = " + Arrays.toString(arr));
-
+        // leftRotate(arr, 4);
+//        leftRotate(arr, 5);
+//        leftRotate(arr, 6);
+        rightRotate(arr, 4);
     }
 
-    /*Function to left rotate arr[] of siz n by d To handle if d >= n */
-    private static void leftRotate(int[] arr, int d, int n) {
-        d = d % n;
+    /* Function to left rotate arr[] of size n by startIndex */
+    private static void leftRotate(int[] arr, int startIndex) {
+        int endIndex = arr.length;
+        startIndex = startIndex % endIndex;
         int i, j, k, temp;
-        int g_c_d = gcd(d, n);
+        int g_c_d = gcd(startIndex, endIndex);
         for (i = 0; i < g_c_d; i++) {
             /* move i-th values of blocks */
             temp = arr[i];
             j = i;
             while (true) {
-                k = j + d;
-                if (k >= n)
-                    k = k - n;
-                if (k == i)
-                    break;
+                k = j + startIndex;
+                if (k >= endIndex) k = k - endIndex;
+                if (k == i) break;
                 arr[j] = arr[k];
                 j = k;
             }
             arr[j] = temp;
         }
+        System.out.println("Left Rotated Array from Element " + startIndex + " is = " + Arrays.toString(arr));
     }
 
-    /*Function to get gcd of a and b*/
+    /* Function to right rotate arr[] of size n by startIndex */
+    private static void rightRotate(int[] arr, int startIndex) {
+        int endIndex = arr.length;
+        startIndex = startIndex % endIndex;
+        int i, j, k, temp;
+        int g_c_d = gcd(startIndex, endIndex);
+        for (i = 0; i < g_c_d; i++) {
+            /* move i-th values of blocks */
+            temp = arr[i];
+            j = i;
+            while (true) {
+                k = j + startIndex;
+                if (k >= endIndex) k = k - endIndex;
+                if (k == i) break;
+                arr[j] = arr[k];
+                j = k;
+            }
+            arr[j] = temp;
+        }
+        System.out.println("Right Rotated Array from Element " + startIndex + " is = " + Arrays.toString(arr));
+    }
+
+    /*Function to get gcd of a and b
+     what is gcd? gcd is the greatest common divisor of two numbers eg. gcd(4, 6) = 2 and gcd(4, 7) = 1 */
     static int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        else
-            return gcd(b, a % b);
+        if (b == 0) return a;
+        else return gcd(b, a % b);
     }
 
     private static void rotate(int[] arr) {
@@ -56,6 +78,18 @@ public class CyclicallyRotateArray {
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
+        }
+    }
+
+    /* Function to reverse arr[] from startIndex to endIndex */
+    private static void reverse(int[] arr, int startIndex, int endIndex) {
+        int temp;
+        while (startIndex < endIndex) {
+            temp = arr[startIndex];
+            arr[startIndex] = arr[endIndex];
+            arr[endIndex] = temp;
+            startIndex++;
+            endIndex--;
         }
     }
 }
