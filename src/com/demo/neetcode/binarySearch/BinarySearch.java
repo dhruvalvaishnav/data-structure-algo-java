4fadd3c74288405be1bc5b57b20da77857d77314
Dhruval Vaishnav <dvaishnav@adobe.com>
2024-10-04 19:47:13 +0530

    update BinarySearch.java

--- src/com/demo/neetcode/binarySearch/BinarySearch.java
+++ src/com/demo/neetcode/binarySearch/BinarySearch.java
@@ -28,7 +28,13 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 12;
-        System.out.println(searchElement(nums, target));
+        System.out.println("Index of 12 in nums: " + searchElement(nums, target));
+        // 2^31 elements scenario
+        int[] largeArr = new int[10000];
+        for (int i = 0; i < largeArr.length; i++) {
+            largeArr[i] = i;
+        }
+        System.out.println("Index of 1000 in large array: " + searchElementInLargeArray(largeArr, 1000));
     }
 
     public static int searchElement(int[] nums, int target) {
@@ -45,7 +51,27 @@ public class BinarySearch {
         }
         return -1;
     }
+
+
+    // what if the given array is -> 2^31 -> then finding the mid should be overflow
+    // solution for that is to calculate the mid like : int mid = left + ((right - left) / 2);
+    // give me the example of above scenario and write a different function for this
+    public static int searchElementInLargeArray(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2); // to avoid overflow, this is the correct way to calculate mid-point in large array
+            if (arr[mid] > target) {
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
 }
 
-// what if the given array is -> 2^31 -> then finding the mid should be overflow
-// solution for that is to calculate the mid like : int mid = left + ((right - left) / 2);
+
