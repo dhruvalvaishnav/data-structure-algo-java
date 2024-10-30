ff385cd01a81667c08839eb5a7500ef8a04dd1df
Dhruval Vaishnav <dvaishnav@adobe.com>
2024-10-30 17:28:30 +0530

    MajorityElement_I

--- src/com/demo/leetcode/MajorityElement_I.java
+++ src/com/demo/leetcode/MajorityElement_I.java
@@ -21,6 +21,8 @@ Follow-up: Could you solve the problem in linear time and in O(1) space?
  */
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MajorityElement_I {
     public static void main(String[] args) {
@@ -28,16 +30,35 @@ public class MajorityElement_I {
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
         System.out.println("majority element in given array is : " + majorityElement(nums));
         System.out.println("majority element in given array is : " + majorityElementUsingBoyerMoore(nums));
+        System.out.println("majority element in given array is : " + majorityElementUsingStreams(nums));
     }
 
-    //  space : O(1) , time : O(n)
+    private static String majorityElementUsingStreams(int[] nums) {
+        // find the majority element using java 8 streams
+        return Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(x -> x.getValue() > nums.length / 2).map(Map.Entry::getKey).findFirst().get().toString();
+        // time complexity : O(n) because of stream operations
+        // space complexity : O(n) because of groupingBy and counting
+    }
+
+    //  space : O(1) because of constant space used as result and count variables
+    //  time : O(n) because of single pass through the array
+    // which algorithm is used here : Boyer-Moore Voting Algorithm
+    // https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+    // what is the intuition behind this algorithm :
+    // 1. If we cancel out each occurrence of an element e with all the other elements that are different from e then e will exist till end if it is a majority element.
+    // 2. If we find two elements that are different, then we can remove them from the list.
+    // 3. This approach will ensure that the majority element is always left over as the last element.
     private static int majorityElementUsingBoyerMoore(int[] nums) {
-        int res = 0, count = 0;
-        for (int n : nums) {
-            if (count == 0) res = n;
-            count += n == res ? 1 : -1;
+        int result = 0, count = 0;
+        for (int currentElement : nums) {
+            // if the count is 0, set the current element as the majority element candidate
+            if (count == 0) {
+                result = currentElement;
+            }
+            // if the current element is the majority element candidate, increment the count
+            count += (result == currentElement) ? 1 : -1;
         }
-        return res;
+        return result;
     }
 
     //  space : O(n) , time : O(n)
@@ -56,5 +77,17 @@ public class MajorityElement_I {
         }
         return -1;
     }
+    /*
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+        return -1;
+    }
+     */
 
 }
