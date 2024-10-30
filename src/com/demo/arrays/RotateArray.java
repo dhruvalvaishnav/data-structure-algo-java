7f6d7e2af30447debb8c11673743dfd43b740c0b
Dhruval Vaishnav <dhruvrajx3@gmail.com>
2024-10-30 21:56:21 +0530

    rotate-array

--- src/com/demo/arrays/RotateArray.java
+++ src/com/demo/arrays/RotateArray.java
@@ -25,6 +25,21 @@ public class RotateArray {
     }
 
     public static void rotateArray(int[] nums, int k) {
+        // in-place without using any extra space
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 
 
